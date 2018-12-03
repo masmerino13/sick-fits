82da04467e88a76e2e1a32ff15ad1407c44922a0
masmerino <masmerino@gmail.com>
2018-12-03 05:42:51 -0600

    Prisma flow

---
+++ backend/src/db.js
@@ -0,0 +1,12 @@
+// connect to remote prisma DB
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.SECRET,
+  debug: false
+});
+
+
+module.exports = db;
